a8db3c0df2176430be3b63ae6c2aa4ed45a52aa0
huyNguyen12620 <huynguyen1262000@gmail.com>
2023-02-13 17:41:04 +0700

    JS4

--- jsbuoi3/index.js
+++ jsbuoi3/index.js
@@ -66,3 +66,97 @@ document.getElementById('btnTinhTriTuyetDoi').onclick = function () {
     }
     document.getElementById('ketQua1').innerHTML = output;
 }
+
+// Ví dụ 2: Cho phép người dùng nhập vào 2 số => in ra số lớn nhất
+
+document.getElementById('btnInSoLonNhat').onclick =function () {
+    // input: soThu1: number, soThu2:number
+    var soThu1 = Number(document.getElementById('soThu1').value);
+    var soThu2 = Number(document.getElementById('soThu2').value);
+    // output: max: number
+    var max = 0;
+    if(soThu1 > soThu2){
+        max = soThu1;
+    }else {
+        max = soThu2;
+    }
+    document.getElementById('ketQua2').innerHTML = 'số lớn nhất: ' + max;
+}
+
+
+// Ví dụ 3: Viết chương trình cho phép người dùng nhập vào điểm toán, điểm lý, điểm hóa -> tính điểm trung bình và xếp loại theo công thức
+
+document.getElementById('btnXepLoai').onclick = function () {
+    var diemToan = Number(document.getElementById('diemToan').value);
+    var diemLy = Number(document.getElementById('diemLy').value);
+    var diemHoa = Number(document.getElementById('diemHoa').value);
+
+    var DTB = (diemToan + diemLy + diemHoa)/3;
+    var xepLoai = '';
+
+    if(DTB < 5){
+        xepLoai =' học lực yếu';
+    }else if(DTB >= 5 && DTB < 6.5){
+        xepLoai =' học lực trung bình';
+    }else if(DTB >=6.5 && DTB < 8){
+        xepLoai =' học lực khá';
+    }else if(DTB > 8 && DTB < 10) {
+        xepLoai=' học lực giỏi'
+    }else{
+        xepLoai=' Không hợp lệ'
+    }
+    document.getElementById('ketQua3').innerHTML ='Điểm trung bình: '+ Math.round(DTB) +xepLoai;
+}
+
+// Bài tập 1: Viết chương trình cho phép người dùng nhập vào số giờ làm và lương / 1h. Tính tiền lương theo công thức sau
+
+document.getElementById('btnTinhLuong').onclick = function () {
+    var soGioLam = Number(document.getElementById('soGioLam').value);
+    var luongCB = Number(document.getElementById('luongCB').value);
+
+    var tienLuong = 0;
+
+    if(soGioLam <= 40){
+        tienLuong = soGioLam * luongCB;
+    }else if(soGioLam > 40){
+        tienLuong = (luongCB * 40) + (luongCB * 1.5 * (soGioLam -40)) ;
+    }else{
+        tienLuong ='Không hợp lệ'
+    }
+    document.getElementById('ketQua_b1').innerHTML = 'Tổng tiền lương = ' +tienLuong.toLocaleString();
+
+}
+
+// var n=5;
+// var ketQua ='';
+// if(n % 2 ===0){
+//     ketQua = 'Số chẵn';
+// }else{
+//     ketQua = 'Số lẻ';
+// }
+
+// Cách viết tắt
+// ketQua = n%2===0 ? 'Số chẵn':'Số lẻ';
+
+document.getElementById('btnTinhTongTien').onclick = function(){
+    var tenSanPham = document.getElementById('tenSanPham').value;
+    var soLuongNhap = Number(document.getElementById('soLuongSanPham').value);
+    var donGiaNhap = Number(document.getElementById('donGiaNhap').value);
+
+    var tongTien = 0;
+
+    if(soLuongNhap <= 50){ //Nếu số lượng bé hơn hoặc = 50 sp
+        tongTien = soLuongNhap * donGiaNhap;
+    }else if(soLuongNhap > 50 && soLuongNhap <=100){
+        // Giảm 8% => tiền gốc *92%
+    tongTien = (soLuongNhap * donGiaNhap) * 92/100;
+    }else if(soLuongNhap > 100){
+        tongTien = (soLuongNhap * donGiaNhap) * 88/100
+    }else{
+        tongTien ='Không hợp lệ'
+    }
+    
+    // In ra
+    document.getElementById ('ketQua_b2').innerHTML = 'Tên SP: ' + tenSanPham + ' - Tổng tiền nhập: ' + tongTien;
+}  
+
